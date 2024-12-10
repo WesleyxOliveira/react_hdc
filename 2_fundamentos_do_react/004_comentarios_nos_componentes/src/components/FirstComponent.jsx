585ecf6dec774c08828645fbb76c938946117628
Wesley <wesleyxoliveira@gmail.com>
2024-12-10 10:34:07 -0300

    Aprendi a fazer comentários no react

---
+++ 2_fundamentos_do_react/004_comentarios_nos_componentes/src/components/FirstComponent.jsx
@@ -0,0 +1,22 @@
+//Arquivos de estilo
+
+function firstComponent() {  
+    //Essa função faz isso
+
+    /*
+      Este
+      é 
+      um
+      comentário de 
+      multiplas 
+      linhas
+    */
+    return (      
+      <>
+      {/*Este tipo de comentário precisa ter um elemento pai, assim como o h2 logo abaixo, por isso é colocado dentro da tag de abertura e fechamento e tem que estar entre chaves.*/}
+        <h2 className="teste">Meu primeiro componente</h2>
+      </>
+    )
+  }
+
+export default firstComponent;
