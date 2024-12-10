8e4b8311a6bd11ff266d24bd7edb89af97eb87db
Wesley <wesleyxoliveira@gmail.com>
2024-12-10 09:28:07 -0300

    Aprendi a importar components

---
+++ 2_fundamentos_do_react/002_importando_components/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+//components
+import FirstComponent from './components/FirstComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Fundamentos do React</h1>
+      <FirstComponent />
+    </>
+  )
+}
+
+export default App
---
+++ 2_fundamentos_do_react/002_importando_components/src/components/FirstComponent.jsx
@@ -0,0 +1,10 @@
+
+function firstComponent() {  
+    return (
+      <>
+        <h2>Meu primeiro componente</h2>
+      </>
+    )
+  }
+
+export default firstComponent;
