938d3fa46dc0ac182e0e10e3b84ebbd2547485fc
Wesley <wesleyxoliveira@gmail.com>
2024-12-17 21:24:33 -0300

    comecei a aprender sobre previous state

---
+++ 3_avancando_em_react/007_previous_state/src/components/ListRender.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+const ListRender = () => {
+
+    const [list] = useState(['Wesley', 'Juliana', 'Érika', 'João']);
+
+    const [users, setUsers] = useState([
+        {id: 1, name: 'Wesley', age: 33},
+        {id: 2, name: 'João', age: 19},
+        {id: 3, name: 'Érika', age: 18}
+    ])
+
+    const deleteRandom = ()=> {
+        const randomNumber = Math.floor(Math.random * 4);
+
+        setUsers((prevUsers)=> {
+            return prevUsers.filter((user)=> randomNumber !== user.id);
+        })
+    }
+
+    return (
+        <div>
+            <ul>
+                {list.map((item, i)=> (
+                    <li key={i}>{item}</li>
+                ))}
+            </ul>
+
+            <ul>
+                {users.map((user)=> (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+
+            <button onClick={deleteRandom}>Delete random user</button>
+        </div>
+    )
+}
+
+export default ListRender
