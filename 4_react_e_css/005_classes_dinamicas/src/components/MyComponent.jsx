d92768ecb80efdb2e8561d1b5e7e870f3393305e
Wesley <wesleyxoliveira@gmail.com>
2025-01-13 10:56:49 -0300

    Aprendi classes dinâmicas no react

---
+++ 4_react_e_css/005_classes_dinamicas/src/components/MyComponent.jsx
@@ -0,0 +1,13 @@
+import './MyComponent.css';
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>CSS De component</h1>
+        <p>Este é o parágrafo do component</p>
+        <p className='my-comp-paragraph'>Este também é do componente</p>
+    </div>
+  )
+}
+
+export default MyComponent
