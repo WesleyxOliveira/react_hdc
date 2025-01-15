8c3eadf7783794ef1882d1d791cc403142bdac06
Wesley <wesleyxoliveira@gmail.com>
2025-01-14 21:23:37 -0300

    Finalizei a tarefa do múdulo de css

--- 4_react_e_css/007_challengecss/src/App.jsx
+++ 4_react_e_css/007_challengecss/src/App.jsx
@@ -4,19 +4,22 @@ import CarDetails from './components/CarDetails'
 
 function App() {
   const [carDetails] = useState([
-    {id: 1, brand: 'Fiat', model: 'Strada', color: 'Branca'},
-    {id: 2, brand: 'VW', model: 'GOL', color: 'Vermelho'},
-    {id: 3, brand: 'BYD', model: 'Dolphin', color: 'Cinza'},
-    {id: 4, brand: 'Ford', model: 'Focus', color: 'Preto'},
+    { id: 1, brand: 'Fiat', model: 'Strada', color: 'Branca' },
+    { id: 2, brand: 'VW', model: 'GOL', color: 'Vermelho' },
+    { id: 3, brand: 'BYD', model: 'Dolphin', color: 'Cinza' },
+    { id: 4, brand: 'Ford', model: 'Focus', color: 'Preto' },
   ])
 
   return (
     <>
-      {
-        carDetails.map((car)=> (
-          <CarDetails key={car.id} brand={car.brand} model={car.model} color={car.color}/>
-        ))
-      }
+      <h1>CarDetails</h1>
+      <div className='list_container'>
+        {
+          carDetails.map((car) => (
+            <CarDetails key={car.id} brand={car.brand} model={car.model} color={car.color} />
+          ))
+        }
+      </div>
     </>
   )
 }
--- 4_react_e_css/007_challengecss/src/components/CarDetails.jsx
+++ 4_react_e_css/007_challengecss/src/components/CarDetails.jsx
@@ -2,13 +2,13 @@ import styles from './CarDetails.module.css';
 
 const CarDetails = ({ brand, model, color }) => {
     return (
-        <div className={styles.cars}>
+        <>
             <ul>
                 <li>Marca: {brand}</li>
                 <li>Modelo {model}</li>
                 <li>Cor: {color}</li>
             </ul>
-        </div>
+        </>
     )
 }
 
