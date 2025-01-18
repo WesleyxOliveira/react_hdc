66fbb5ef6ad41eef9542d3d14c58aabf934ba898
Wesley <wesleyxoliveira@gmail.com>
2025-01-18 16:26:44 -0300

    Aprendi a fazer alteração de state inline nos inputs com react

---
+++ 5_formularios_com_react/004_alteracao_de_state_inline/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import MyForm from './components/MyForm'
+
+function App() {
+  return (
+    <>
+      {/*Criação de Form */}
+      <h2>Forms</h2>
+      <MyForm />
+    </>
+  )
+}
+
+export default App
