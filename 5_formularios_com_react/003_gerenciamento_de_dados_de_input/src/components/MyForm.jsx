c035e8a3503d867e3c14bf22dd1402f2b3d6340e
Wesley <wesleyxoliveira@gmail.com>
2025-01-19 13:37:22 -0300

    Aprendi a enviar dados de um formulário para o backend atraveés do react

---
+++ 5_formularios_com_react/003_gerenciamento_de_dados_de_input/src/components/MyForm.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import './MyForm.css';
+
+const MyForm = () => {
+
+    //3- Gerenciamento de dados
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const handleChange = (e)=> {
+        setName(e.target.value);
+    }
+
+    console.log(name);
+
+    return (        
+        <>
+            {/* 1- Criação de Form */}
+            <form>
+                <label htmlFor="name">Nome: </label>
+                <input type="text" name='name' placeholder='Digite o seu nome' onChange={handleChange} id='name'/>
+
+                {/* 2- Label envolvendo o input */}
+                <label>
+                    <span>E-mail</span>
+                    <input type="email" name="email"/>
+                </label>
+
+                <input type="submit" value="Enviar" />
+            </form>
+        </>
+    )
+}
+
+export default MyForm
---
+++ 5_formularios_com_react/005_envio_de_form/src/components/MyForm.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import './MyForm.css';
+
+const MyForm = () => {
+    {/* 5 - Envio de form */}
+    // 3- Gerenciamento de dados
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const handleChange = (e)=> {
+        setName(e.target.value);
+    }
+
+    // console.log(name);
+    // console.log(email);
+
+    const handleSubmit = (e)=> {
+        e.preventDefault();
+        console.log(name);
+        console.log(email);
+    }
+
+    return (        
+        <>
+            {/* 1- Criação de Form */}
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Nome: </label>
+                <input type="text" name='name' placeholder='Digite o seu nome' onChange={handleChange} id='name'/>
+
+                {/* 2- Label envolvendo o input */}
+                <label>
+                    <span>E-mail</span>
+                    {/* 4 - Simplificação de manipulação de state */}
+                    <input type="email" name="email" onChange={(e)=> setEmail(e.target.value)} autoComplete='off'/>
+                </label>
+
+                <input type="submit" value="Enviar"/>
+            </form>
+        </>
+    )
+}
+
+export default MyForm
