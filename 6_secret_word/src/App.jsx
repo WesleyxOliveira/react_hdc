55c8b6ed508afeeaba21357cb47f52ba9cb3bd8c
Wesley <wesleyxoliveira@gmail.com>
2025-02-03 10:38:33 -0300

    Criei a funcionalidade de escolher a palavra e categoria de forma aleatória.

--- 6_secret_word/src/App.jsx
+++ 6_secret_word/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import { userCallback, useEffect, useState } from 'react';
 
 //data
-import {wordsList} from './data/words'
+import { wordsList } from './data/words'
 
 //components
 import StartScreen from './components/StartScreen'
@@ -13,35 +13,68 @@ import Game from './components/Game';
 import GameOver from './components/GameOver';
 
 const stages = [
-  {id: 1, name: 'start'},
-  {id: 2, name: 'game'},
-  {id: 3, name: 'end'}
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' }
 ];
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState('');
+  const [pickedCateroy, setPickedCategory] = useState('');
+  const [letters, setPickedLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    //Pick a random category
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    //Pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+    console.log(category);
+    console.log(word);
+
+    return { word, category };
+  }
+
   //starts the secret word game
-  const startGame = ()=> {
+  const startGame = () => {
+    //pick word and pick category
+    const { word, category } = pickWordAndCategory();
+    console.log(word, category);
+
     setGameStage(stages[1].name);
+
+    //create an array of letters
+    let wordLetters = word.split('')
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+    console.log(wordLetters);
+
+    //fill states
+    setPickedCategory(word);
+    setPickedCategory(category);
+    setPickedLetters(letters);
   }
 
   //process the letter input
-  const verifyLetter = ()=> {
+  const verifyLetter = () => {
     setGameStage(stages[2].name);
   }
 
   //restarts the game
-  const retry = ()=> {
+  const retry = () => {
     setGameStage(stages[1].name);
   }
 
   return (
     <div className='App'>
-      {gameStage === 'start' && <StartScreen startGame={startGame}/> }
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
-      {gameStage === 'end' && <GameOver retry={retry}/>}
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   )
 }
