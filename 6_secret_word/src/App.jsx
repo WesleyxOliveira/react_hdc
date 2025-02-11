322a2b109fad5f9ce20b2e5fe60a0f6915176251
Wesley <wesleyxoliveira@gmail.com>
2025-02-10 21:00:44 -0300

    Criei a função de processamento de letras válidas e inválidas

--- 6_secret_word/src/App.jsx
+++ 6_secret_word/src/App.jsx
@@ -67,8 +67,32 @@ function App() {
 
   //process the letter input
   const verifyLetter = (letter) => {
-    console.log(letter)
+
+    const normalizedLetter = letter.toLowerCase();
+
+    {
+      //check if letter has already been utilized
+      if (guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)) {
+        return;
+      }
+
+      //push guessed letter or remove a guess
+      if (letters.includes(normalizedLetter)) {
+        setGuessedLetters((actualGuessedLetters) => [
+          ...actualGuessedLetters,
+          normalizedLetter
+        ])
+      } else {
+        setWrongLetters((actualWrongLetters) => [
+          ...actualWrongLetters,
+          normalizedLetter
+        ]);
+      }
+    }
   }
+
+  console.log(guessedLetters);
+  console.log(wrongLetters);
 
   //restarts the game
   const retry = () => {
@@ -78,15 +102,15 @@ function App() {
   return (
     <div className='App'>
       {gameStage === 'start' && <StartScreen startGame={startGame} />}
-      {gameStage === 'game' && <Game 
-      verifyLetter={verifyLetter} 
-      pickedWord={pickedWord}
-      pickedCategory={pickedCategory}
-      letters={letters}
-      guessedLetters={guessedLetters}
-      wrongLetters={wrongLetters}
-      guesses={guesses}
-      score={guesses}
+      {gameStage === 'game' && <Game
+        verifyLetter={verifyLetter}
+        pickedWord={pickedWord}
+        pickedCategory={pickedCategory}
+        letters={letters}
+        guessedLetters={guessedLetters}
+        wrongLetters={wrongLetters}
+        guesses={guesses}
+        score={guesses}
       />}
       {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
