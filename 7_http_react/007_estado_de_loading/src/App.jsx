daf96b07e644a1b1dd2e4fccae747e678ef3a868
Wesley <wesleyxoliveira@gmail.com>
2025-04-06 16:46:38 -0300

    Criei o estado de laoding para a funçaõ de adicionar produto

---
+++ 7_http_react/007_estado_de_loading/src/App.jsx
@@ -0,0 +1,90 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+// 4 - custom hook
+import { useFetch } from './hooks/useFetch';
+
+function App() {
+  const url = 'http://localhost:3000/products';  
+
+  const [products, setProducts] = useState([]);
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  
+  // 4 - custom hook
+  const { data: items, httpConfig, loading } = useFetch(url);
+
+  // 1 - resgatando datos
+  // useEffect(()=> {
+  //   async function fetchData() {
+  //     const res = await fetch(url);
+  //     const data = await res.json();
+
+  //     setProducts(data);
+  //   }
+
+  //   fetchData();
+  // }, []); 
+
+  // 2 - add de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price
+    }
+
+    // const res = await fetch(url, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-type': 'application/json'
+    //   },
+    //   body: JSON.stringify(product)
+    // })
+
+    // const addedProduct = await res.json();
+
+    // // 3 - carregamento dinâmico
+    // setProducts((previousProducts) => [...previousProducts, addedProduct]);
+
+    // 5 - Refatorando post
+    httpConfig(product, 'POST');
+    setName('');
+    setPrice('');
+  }
+
+  return (
+    <>
+      <h1>Lista de produtos</h1>
+
+      {/* 6 - loading */}
+      {loading && <p>Carregando dados...</p>}
+
+      {
+        !loading && <ul>
+        {
+          items && items.map((product) => (
+            <li key={product.id}>{product.name} R$: {product.price}</li>
+          ))
+        }
+      </ul>
+      }
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome: <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+
+          <label>
+            Price: <input type="number" name="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+          </label>
+
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default App
