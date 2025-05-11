a66c224a8360c2b2382138ba52a437d56e3cbf4b
wesley <wesleyxoliveira@gmail.com>
2025-05-11 11:45:09 -0300

    Criei e adicionei no projeto os componentes de navbar e footer

--- 10_miniblog/src/App.jsx
+++ 10_miniblog/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
-      <h1>MiniBlog</h1>
+      <div className="container">
+        <Navbar />
+        <Outlet />
+        
+      </div>
+<Footer />
     </div>
   )
 }
--- 10_miniblog/src/main.jsx
+++ 10_miniblog/src/main.jsx
@@ -3,8 +3,32 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+// pages
+import Home from './routes/Home/Home.jsx'
+import About from './routes/About/About.jsx'
+
+// router config
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/about',
+        element: <About />
+      }
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
