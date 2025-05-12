81b327c54165a8b8f54563fc470f7b1e3cb4a66d
wesley <wesleyxoliveira@gmail.com>
2025-05-12 15:00:36 -0300

    Criei a funcionalidade de registrar um usuário no firebase autentication

--- 10_miniblog/src/routes/Register/Register.jsx
+++ 10_miniblog/src/routes/Register/Register.jsx
@@ -1,3 +1,4 @@
+import { useAuthentication } from '../../hooks/useAuthentication';
 import styles from './Register.module.css'
 import { useState, useEffect } from 'react'
 
@@ -8,7 +9,9 @@ const Register = () => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
 
-  const handleSubmit = (e) => {
+  const {createUser, error: authError, auth, loading} = useAuthentication();
+
+  const handleSubmit = async(e) => {
     e.preventDefault();
 
     setError('');
@@ -24,8 +27,16 @@ const Register = () => {
       return;
     }
 
+    const res = await createUser(user);
+
     console.log(user);
   }
+
+  useEffect(()=> {
+    if(authError) {
+      setError(authError);
+    }
+  }, [authError])
 
   return (
     <div className={styles.register}>
@@ -78,7 +89,8 @@ const Register = () => {
               setConfirmPassword(e.target.value)} />
         </label>
 
-        <button className='btn'>Cadastrar</button>
+        {!loading && <button className='btn'>Cadastrar</button>}
+        {loading && <button className='btn' disabled>Aguarde...</button>}
         {error && <p className='error'>{error}</p>}
       </form>
     </div>
